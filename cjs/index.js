3af30d937e75b3b0686d5b335742574878600d55
webreflection <andrea.giammarchi@gmail.com>
2023-04-25 16:37:58 +0200

    Added JSDoc TS to exports

--- cjs/index.js
+++ cjs/index.js
@@ -1,8 +1,28 @@
 'use strict';
+/**
+ * Given a CSS selector, returns the first matching node.
+ * @param {string} css the CSS selector to query
+ * @param {Document | DocumentFragment | Element} [root] the optional parent node to query
+ * @returns {Element?} the found element, if any
+ */
 const $ = (css, root = document) => root.querySelector(css);
 exports.$ = $;
+
+/**
+ * Given a CSS selector, returns a list of all matching nodes.
+ * @param {string} css the CSS selector to query
+ * @param {Document | DocumentFragment | Element} [root] the optional parent node to query
+ * @returns {Element[]} a list of found nodes
+ */
 const $$ = (css, root = document) => [...root.querySelectorAll(css)];
 exports.$$ = $$;
+
+/**
+ * Given a XPath selector, returns a list of all matching nodes.
+ * @param {string} path the XPath selector to evaluate
+ * @param {Document | DocumentFragment | Element} [root] the optional parent node to query
+ * @returns {Node[]} a list of found nodes (elements, attributes, text, comments)
+ */
 const $x = (path, root = document) => {
   const expression = (new XPathEvaluator).createExpression(path);
   const xpath = expression.evaluate(root, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE);
